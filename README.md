6873f39e608917a13c4e111c86b31088b1ca4b4f
Fred Moyer <fred@redhotpenguin.com>
2021-01-14 21:58:17 -0800

    tracemate beta

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # nanotron
+
+## takes JSON trace data, transforms it into protobuf or other format
+## currently setup for [tracemate](https://github.com/MajorLeagueBaseball/tracemate/) traces
+
+```
+package main
+import (
+    tm "github.com/redhotpenguin/nanotron/tracemate'
+    "io/ioutil"
+    "fmt"
+)
+
+func main() {
+    ct, _ := ioutil.Readfile("tracemate.json")
+    proto := tm.JsonToProto(ct)
+    fmt.Println("proto conversion is ", proto) 
+}
+```
---
+++ tracemate/nanotron.go
@@ -0,0 +1,64 @@
+package tracemate
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	//	v11 "github.com/open-telemetry/opentelemetry-proto/gen/go/common/v1"
+	ott "github.com/open-telemetry/opentelemetry-proto/gen/go/trace/v1"
+	"time"
+)
+
+// trace
+type Trace struct {
+	Timestamp string `json:"@timestamp"`
+	Span      struct {
+		Id             string
+		Transaction_id string
+		Trace_id       string
+		Parent_id      string
+		Name           string
+		Duration       struct {
+			US int
+		}
+		Http struct {
+			Url struct {
+				Original string
+			}
+			Response struct {
+				Status int
+			}
+		}
+	}
+}
+
+func JsonToProto(ct []byte) *ott.Span {
+	var trace Trace
+	err := json.Unmarshal(ct, &trace)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("json is ", trace)
+
+	var oSpan ott.Span
+
+	oSpan.SpanId = []byte(trace.Span.Id)
+	oSpan.TraceId = []byte(trace.Span.Trace_id)
+	oSpan.ParentSpanId = []byte(trace.Span.Parent_id)
+	oSpan.Name = trace.Span.Name
+
+	timeStart, _ := time.Parse(time.RFC3339, trace.Timestamp)
+	oSpan.StartTimeUnixNano = uint64(timeStart.UnixNano())
+
+	// parse the microsecond duration, add to the start
+	u, _ := time.ParseDuration(fmt.Sprintf("%dµs", trace.Span.Duration.US))
+	oSpan.EndTimeUnixNano = uint64(timeStart.Add(u).UnixNano())
+
+	// WIP
+	// oSpan.Attributes = []*v11.KeyValue{"status": trace.Span.Http.Response.Status}
+
+	// serialize
+	proto.Marshal(&oSpan)
+
+	return &oSpan
+}
